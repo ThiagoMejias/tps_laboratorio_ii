21445fa37d19aebad92f1ff4a12c5e94112fd0a2
ThiagoMejias <mejiasthiago1@gmail.com>
2022-05-24 18:08:16 -0300

    Primer push

---
+++ Tp_03/Entidades/Bebida.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public enum tipo
+    {
+        vino, agua, cerveza, gaseosa
+    }
+
+    public class Bebida : Producto
+    {
+
+        protected int capacidad;
+        protected tipo tipo;
+
+        public Bebida(int id, float precio, string descripcion, int capacidad, tipo tipo, float cantidad)
+            : base(id, precio, descripcion, cantidad)
+        {
+            this.capacidad = capacidad;
+            this.tipo = tipo;
+
+        }
+        private string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ToString());
+            sb.AppendLine($"Capacidad:{this.capacidad}");
+            sb.AppendLine($"Tipo:{this.tipo}");
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return this.Mostrar();
+        }
+
+    }
+}
